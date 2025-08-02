e7350fd02a62d2e94bc1a03490d115b634c7b86b
Freddy Rivero <fmroldanrivero@gmail.com>
2025-08-02 10:10:17 +0100

    map feature dev

--- src/__main__.py
+++
@@ -1,7 +0,0 @@
-# src/__main__.py
-
-from ..app import app
-
-if __name__ == "__main__":
-    # Note: debug=True is for development. Turn it off for production.
-    app.run(host='0.0.0.0', port=5000, debug=True)
